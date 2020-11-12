54e1230091695f306876a3b98932c86e6a04a109
Max Palehin <ars_kit@icloud.com>
2020-11-12 19:04:33 +0300

    /

--- lab1.2./main.cpp
+++ lab1.2./main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //18 вариант
 // класс школьника
 class SchoolMan {
-
+    
 private:
     string name;
     string lastName;
@@ -16,7 +16,7 @@ private:
     string adress;
 public:
     SchoolMan(){}
-
+    
     SchoolMan(string name, string lastName, string sex, int group, string dateOfBorn, string adress) {
         this->name = name;
         this->lastName = lastName;
@@ -24,9 +24,9 @@ public:
         this->group = group;
         this->dateOfBorn = dateOfBorn;
         this->adress = adress;
-
+        
     }
-
+    
     string getName() {
         return name;
     }
@@ -45,53 +45,62 @@ public:
     string getAdress() {
         return adress;
     }
-
+    
     friend ostream& operator<<(ostream& os, const SchoolMan& schoolMan)
     {
         os << "" << schoolMan.name << " | " << schoolMan.lastName << " | " << schoolMan.sex << " | "
-            << schoolMan.group << " | " << schoolMan.dateOfBorn << " | " << schoolMan.adress << "\n";
+        << schoolMan.group << " | " << schoolMan.dateOfBorn << " | " << schoolMan.adress << "\n";
         return os;
     }
     bool operator==(SchoolMan const& other) const {
         return ((name == other.name) && (lastName == other.lastName) && (sex == other.sex) &&
-            (group == other.group) && (dateOfBorn == other.dateOfBorn) && (adress == other.adress));
+                (group == other.group) && (dateOfBorn == other.dateOfBorn) && (adress == other.adress));
     }
+    friend bool operator > (SchoolMan const& first, SchoolMan const& second){
+        return first.group>second.group;
+    };
+    
+    friend bool operator < (SchoolMan const& first, SchoolMan const & second){
+        return first.group<second.group;
+    };
+    friend bool operator < (SchoolMan const& first, int const& second){
+        return first.group<second;
+    };
+    
+    friend bool operator >= (SchoolMan const& first, SchoolMan const& second){
+        return first.group>=second.group;
+    };
+    friend bool operator <= (SchoolMan const& first, SchoolMan const& second){
+        return first.group<=second.group;
+    };
+    friend int operator%(SchoolMan const& first, SchoolMan const& second){
+        return first.group%second.group;
+    };
 };
 
 template<class T>
 T pop(list<T>& lst) {
-
+    
     auto p = lst.begin();
-
+    
     T elem = *p;
-
+    
     lst.remove(elem);
-
+    
     return elem;
 }
 //добавление с приоритетом
 template<class T>
 void push(list<T>& lst,T elem) {
-
+    
     if (lst.empty()) {
         lst.push_back(elem);
     }
     else {
-        list<SchoolMan>::iterator p = lst.begin();
-
+        auto p = lst.begin();
+        
         while (p != lst.end()) {
-            if ((*p).getGroup() > elem.getGroup()) {
-                break;
-            }
-            else if ((*p).getGroup() == elem.getGroup() && (*p).getDateOfBorn() > elem.getDateOfBorn()) {
-                break;
-            }
-            else if ((*p).getGroup() == elem.getGroup() && (*p).getDateOfBorn() == elem.getDateOfBorn()
-                && (*p).getLastName() > elem.getLastName()) {
-                break;
-            }
-            else if ((*p).getGroup() == elem.getGroup() && (*p).getDateOfBorn() == elem.getDateOfBorn()
-                && (*p).getLastName() == elem.getLastName() && (*p).getName() > elem.getName()) {
+            if (*p > elem) {
                 break;
             }
             p++;
@@ -100,11 +109,11 @@ void push(list<T>& lst,T elem) {
     }
 }
 int main() {
-
+    
     setlocale(LC_ALL, "Rus");
-
+    
     list<SchoolMan> lst;
-
+    
     //заполнение класс школьник
     SchoolMan scm1("Виктория", "Шуманская", "Женский", 10, "07.04.1999", "Рязань");
     SchoolMan scm2("Максим", "Палёхин", "Мужской", 1, "20.12.2001", "Москва");
@@ -113,7 +122,7 @@ int main() {
     SchoolMan scm5("Александр", "Краснов", "Мужской", 3, "08.11.2001", "Ростов");
     SchoolMan scm6("Александр", "Сычев", "Мужской", 2, "04.01.2001", "Ростов");
     SchoolMan scm8("Романов", "Алексей", "Мужской", 9, "23.05.2001", "Домодедово");
-
+    
     //добавление в лист наши объекты
     push(lst, scm1);
     push(lst, scm2);
@@ -127,14 +136,14 @@ int main() {
     SchoolMan scm7 = pop(lst);
     
     list<SchoolMan>::iterator p = lst.begin();
-
+    
     while (p != lst.end()) {
         cout << *p << endl;
         p++;
     }
     cout << "Удаленный элемент: " << endl;
-
+    
     cout << scm7 << endl;
-
+    
     return 0;
 }
